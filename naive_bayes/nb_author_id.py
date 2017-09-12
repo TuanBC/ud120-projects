83a1bde7be901e55ab477c36515eacb337d9f649
TuanBC <tuannguyen091095@gmail.com>
2017-09-12 15:14:41 +0900

    Finish Naive Bayes

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -15,13 +15,20 @@ from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+clf = GaussianNB()
+clf.fit(features_train, labels_train)
 
+pdt_label = clf.predict(features_test)
+print(accuracy_score(labels_test, pdt_label))
 
 
 #########################################################
